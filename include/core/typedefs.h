4f4aab1c24f6dcf93fe2d3a2c77ab8f771722c1c
ubermenzchen <matheus_garcias@hotmail.com>
2019-10-10 18:32:20 -0300

    Nomeando parâmetros

--- include/core/typedefs.h
+++ include/core/typedefs.h
@@ -7,12 +7,12 @@
  *  "Template" for comparison string
  *  Inspired by qsort compare function signature
  */
-typedef int32_t (*comp_f)(void*, void*);
+typedef int32_t (*cmp_f)(void*, void*);
 
 /* 
  *  "Template" for sorting algorithm (to be used or not to be used, that's the question.) 
  *  Inspired by qsort function signature
  */
-typedef void (*sorting_algorithm_f)(void *, size_t, size_t, comp_f);
+typedef void (*sorting_algorithm_f)(void *ptr, size_t num, size_t size, cmp_f cmp);
 
 #endif
