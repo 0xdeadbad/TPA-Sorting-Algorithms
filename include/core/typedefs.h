9555902828e7da3be7b33542d4bbc1c3afaec2d8
ubermenzchen <matheus_garcias@hotmail.com>
2019-10-10 17:53:29 -0300

    fixing a typo

--- include/core/typedefs.h
+++ include/core/typedefs.h
@@ -10,7 +10,7 @@
 typedef int32_t (*comp_f)(void*, void*);
 
 /* 
- *  "Tempalte" for sorting algorithm (to be used or not to be used, that's the question.) 
+ *  "Template" for sorting algorithm (to be used or not to be used, that's the question.) 
  *  Inspired by qsort function signature
  */
 typedef void (*sorting_algorithm_f)(void *, size_t, size_t, comp_f);
