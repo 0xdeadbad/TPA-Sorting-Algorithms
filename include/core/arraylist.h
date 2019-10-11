64430de393fa97c544215050391920c1f78c31d4
ubermenzchen <matheus_garcias@hotmail.com>
2019-10-11 13:58:56 -0300

    movendo pessoa e alterando arraylist para identificar possíveis erros

--- include/core/arraylist.h
+++ include/core/arraylist.h
@@ -12,7 +12,7 @@ typedef struct {
     size_t capacity;
 } arraylist_t, *arraylist_p;
 
-void arraylist_init(arraylist_p a, size_t item_size, size_t  starting_capacity, size_t mul_factor);
-void arraylist_pushback(arraylist_p a, void *v);
+int arraylist_init(arraylist_p a, size_t item_size, size_t  starting_capacity, size_t mul_factor);
+int arraylist_pushback(arraylist_p a, void *v);
 
 #endif
--- src/arraylist.c
+++ src/arraylist.c
@@ -3,20 +3,28 @@
 #include <stdlib.h>
 #include <string.h>
 
-void arraylist_init(arraylist_p a, size_t item_size, size_t  starting_capacity, size_t mul_factor) {
-	a->array = calloc(starting_capacity, item_size);
+int arraylist_init(arraylist_p a, size_t item_size, size_t  starting_capacity, size_t mul_factor) {
+	if((a->array = calloc(starting_capacity, item_size)) == NULL) {
+        return -1;
+    }
 	a->item_size = item_size;
 	a->size = 0;
 	a->capacity = starting_capacity;
 	a->mul_factor = mul_factor;
+
+    return 0;
 }
 
-void arraylist_pushback(arraylist_p a, void *v) {
+int arraylist_pushback(arraylist_p a, void *v) {
 	if(a->size == a->capacity) {
-		a->array = (arraylist_p) realloc(a->array, a->capacity*a->mul_factor*a->item_size);
+		if((a->array = (arraylist_p) realloc(a->array, a->capacity*a->mul_factor*a->item_size)) == NULL) {
+            return -1;
+        }
 		a->capacity *= a->mul_factor;
 		memset((uint8_t*)a->array+(a->item_size*a->size), 0, a->capacity-a->size);
 	}
 
 	memcpy((uint8_t*)a->array+(a->item_size*a->size++), v, a->item_size);
+
+    return 0;
 }
