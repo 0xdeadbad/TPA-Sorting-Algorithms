04d12ed86dda8a8e48421b7e191188fe41fe14f3
ubermenzchen <matheus_garcias@hotmail.com>
2019-10-15 17:50:10 -0300

    Modificações pequenas para passar mesmos parâmetros para funções diferentes

--- src/algorithms/quicksort.c
+++ src/algorithms/quicksort.c
@@ -10,7 +10,7 @@ size_t partition(void *ptr, size_t num, size_t size, cmp_f cmp) {
 
     i = 0;
 
-    for(j = 1; j < num; j++) 
+    for(j = 1; j <= num; j++) 
         if(cmp((uint8_t*)ptr+j*size, ptr) < 0) 
             SWAP((uint8_t*)ptr+j*size, (uint8_t*)ptr+(++i)*size, size);
 
@@ -23,7 +23,7 @@ void quick_sort(void *ptr, size_t num, size_t size, cmp_f cmp) {
     uint8_t *a, *b;
     
     a = ptr;
-    b = (uint8_t*)ptr+(num-1)*size;
+    b = (uint8_t*)ptr+num*size;
     
     if(a < b) {
         int q;
@@ -32,7 +32,7 @@ void quick_sort(void *ptr, size_t num, size_t size, cmp_f cmp) {
         q = partition(ptr, num, size, cmp);
 
         c = (uint8_t*)ptr+(q+1)*size;
-        d = (uint8_t*)ptr+(num-1)*size;
+        d = (uint8_t*)ptr+num*size;
 
         quick_sort(ptr, q-1, size, cmp);
         quick_sort((uint8_t*)ptr+(q+1)*size, (d - c)/size, size, cmp);
