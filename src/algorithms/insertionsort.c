b155a56a4e743c203ed2f0f69952f4c3f7d0223a
ubermenzchen <matheus_garcias@hotmail.com>
2019-10-17 00:01:15 -0300

    oops

--- src/algorithms/insertionsort.c
+++ src/algorithms/insertionsort.c
@@ -7,23 +7,6 @@
 #include "core/typedefs.h"
 #include "core/util.h"
 
-/*
-def insertionSort(l):
-    ''' Ordena os elementos de uma lista em ordem crescente.
-        Para isso, vai inserindo os elementos ordenadamente na própria lista..'''
-    for k in range(1, len(l)):
-        elem = l[k]
-        pos = k
-
-        while pos > 0 and l[pos - 1].uid > elem.uid:
-            l[pos] = l[pos - 1]
-            pos = pos - 1
-
-        l[pos] = elem
-
-    return l
-*/
-
 void insertion_sort(void *ptr, size_t num, size_t size, cmp_f cmp) {
     for(int k = 1; k <= num; k++) {
         void *elem;
