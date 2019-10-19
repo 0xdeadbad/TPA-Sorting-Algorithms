8bc494151026a3a854e80865a88c9be3f1705ac3
ubermenzchen <matheus_garcias@hotmail.com>
2019-10-19 17:57:36 -0300

    Esqueci do .h ...

--- include/algorithms/sortalgorithms.h
+++ include/algorithms/sortalgorithms.h
@@ -10,6 +10,8 @@ void quick_sort(void *ptr, size_t num, size_t size, cmp_f cmp);
 void insertion_sort(void *ptr, size_t num, size_t size, cmp_f cmp);
 void merge_sort(pessoa_t vec[], size_t vecSize, cmp_f cmp);
 void heap_sort(pessoa_t pessoas[], size_t array_size, cmp_f cmp);
+void tim_sort(pessoa_t pessoas[], size_t array_size, cmp_f cmp);
+void intro_sort(void *ptr, size_t num, size_t size, cmp_f cmp);
 
 
 #endif
